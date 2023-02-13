7ccd6ce92715977adae803f1196de88931a7241a
evans ochieng <evansochieng121@gmail.com>
2023-02-13 14:22:17 +0300

    style the nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../index.css"
 
 const NavBar = () => {
+
+  const linkStyles = {
+    display: "inline-block",
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "rgb(12 74 110)",
+    textDecoration: "none",
+    color: "white",
+    borderRadius: "5px",
+  };
+
   return (
-    <div>
-      <Link to="/" exact="true">
-        Home
-      </Link>
-      <Link to="/income" exact="true">
-        Income
-      </Link>
-      <Link to="/expenses" exact="true">
-        Expenses
-      </Link>
-      <Link to="/logout" exact="true">
-        Sign Out
-      </Link>
-    </div>
+    <nav className="navigation">
+      {/* <i className="fa fa-money" style={{fontSize:"24px"}}></i> */}
+      <h2 className='app-name'>
+        Outflows
+      </h2>
+      <div className="navigation-menu">
+        <Link to="/" exact="true" style={linkStyles}>
+          Home
+        </Link>
+        <Link to="/income" exact="true" style={linkStyles}>
+          Income
+        </Link>
+        <Link to="/expenses" exact="true" style={linkStyles}>
+          Expenses
+        </Link>
+        <Link to="/logout" exact="true" style={linkStyles}>
+          Sign Out
+        </Link>
+      </div>
+    </nav>
   );
 }
 
