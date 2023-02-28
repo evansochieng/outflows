9d8782998d4cd18ba34501a5f180ab3e8003d7df
evans ochieng <evansochieng121@gmail.com>
2023-02-28 13:09:12 +0300

    add table header for expense table

--- frontend/src/components/Expenses.js
+++ frontend/src/components/Expenses.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 function Expenses() {
   return (
-    <div>Expenses</div>
-  )
+    <div>
+      <div>
+        <div
+          class="card"
+          style={{ width: "60rem", marginBottom: "30px", marginTop: "30px" }}
+        >
+          <div class="card-body">
+            <div style={{ display: "inline-block", justifyContent: 'space-between' }}>
+              <h5
+                class="card-title"
+                style={{ color: "#0d6efd", float: "left" }}
+              >
+                Expenses
+              </h5>
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                style={{ float: "right" }}
+              >
+                NEW EXPENSE
+              </button>
+            </div>
+            <p class="card-text">Total Expenses: $ {0}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Expenses 
