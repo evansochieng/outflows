5261fc1bf0668b9f34b53b96ef95b205af7208a2
evans ochieng <evansochieng121@gmail.com>
2023-02-28 12:04:01 +0300

    add cards for balance, income, expense in home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  //define variable for balance
+  const [balance, setBalance] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <div class="card" style={{ width: "18rem", marginBottom: "30px" }}>
+          <div class="card-body">
+            <h5 class="card-title">MY BALANCE</h5>
+            <p class="card-text">Total Balance: $ {balance}</p>
+          </div>
+        </div>
+
+        <div class="card" style={{ width: "18rem", marginBottom: "30px" }}>
+          <div class="card-body">
+            <h5 class="card-title">INCOME</h5>
+            <p class="card-text">Total Income: $ {income}</p>
+          </div>
+        </div>
+
+        <div class="card" style={{ width: "18rem", marginBottom: "30px" }}>
+          <div class="card-body">
+            <h5 class="card-title">EXPENSES</h5>
+            <p class="card-text">Total Expenses: $ {expense}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home
