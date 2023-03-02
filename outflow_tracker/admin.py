9fd404a9352da9620b3d39e831c441d3f7d865f7
evans ochieng <evansochieng121@gmail.com>
2023-03-02 06:57:28 +0300

    add expense model

--- outflow_tracker/admin.py
+++ outflow_tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+# Add the expense model to the admin page
+class ExpenseAdmin(admin.ModelAdmin):
+    # list of attributes
+    list = ('name', 'date', 'category', 'description', 'payment_mode', 'value')
+
+# register the site
+admin.site.register(Expense, ExpenseAdmin)
--- outflow_tracker/models.py
+++ outflow_tracker/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+# for enumeration types choices
+from django.utils.translation import gettext_lazy as _
+
+# Create a model for expenses
+class Expense(models.Model):
+
+    # Option 1 of choices
+    # list the categories of expenses
+    SHOPPING = 'Shopping'
+    TRAVEL = 'Travel'
+    MISC = 'Misc'
+    GENERAL_EXPENSE = 'GenExpense'
+    EDUCATION = 'Education'
+
+    # create choices of the categories
+    EXPENSE_CATEGORY_CHOICES = [
+        (SHOPPING, 'Shopping'),
+        (TRAVEL, 'Travel'),
+        (MISC, 'Misc'),
+        (GENERAL_EXPENSE, 'General Expense'),
+        (EDUCATION, 'Education')
+    ]
+
+    # Option 2 of choices
+    class PaymentMode(models.TextChoices):
+        CASH = 'Cash', _('Cash')
+        MPESA = 'Mpesa', _('Mpesa')
+        CARD = 'Card', _('Card')
+        CHEQUE = 'Cheque', _('Cheque')
+
+    # define the attributes of the model
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    category = models.CharField(max_length=100, choices=EXPENSE_CATEGORY_CHOICES, default=SHOPPING)
+    description = models.TextField()
+    payment_mode = models.CharField(max_length=20, choices=PaymentMode.choices, default=PaymentMode.CASH)
+    value = models.IntegerField()
+
+    # default attribute to be displayed
+    def __str__(self):
+        return self.name
