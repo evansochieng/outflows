1d9557476cb1d33923bed91040d3fd5bd3d349b6
evans ochieng <evansochieng121@gmail.com>
2023-03-02 07:19:34 +0300

    update views for the expense model

--- outflow_tracker/views.py
+++ outflow_tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import ExpenseSerializer
+from rest_framework import viewsets
+from .models import Expense
 
-# Create your views here.
+# Create view for Expenses
+class ExpenseView(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    queryset = Expense.objects.all()
