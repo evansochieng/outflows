d257bd7b15975431f181011893a7c9d23894653e
evans ochieng <evansochieng121@gmail.com>
2023-03-03 10:00:26 +0300

    restyle login page

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -20,40 +20,58 @@ const Logout = ({ setIsLogin }) => {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "50px"
-      }}
-    >
-      <div>
-        <p>
-          <span>Are you sure you want to log out?</span>
-        </p>
-        <p>
-          You won't be able to track your expenditure man! You might waste
-          yourself man!
-          <ImSad style={{ color: "#fcd34d", fontSize: "50px" }} />
-        </p>
-      </div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <button
-          style={{
-            width: "150px",
-            backgroundColor: "#e96916",
-            marginBottom: "20px",
-          }}
-          onClick={logoutUser}
-        >
-          LOG OUT
-        </button>
-        <button style={{ width: "150px" }} onClick={redirectHome}>
-          CANCEL
-        </button>
-      </div>
+    <div>
+      {/* new logout */}
+      <section className="vh-100">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div
+                className="card shadow-2-strong"
+                style={{ borderRadius: "1rem" }}
+              >
+                <div className="card-body p-5 text-center">
+                  <h3 className="mb-5">Sign out</h3>
+
+                  <div>
+                    <p className='mb-5'>
+                      <span>Are you sure you want to log out?</span>
+                    </p>
+                    <p>
+                      You won't be able to track your expenditure man! You might
+                      waste your money!
+                      <ImSad style={{ color: "#fcd34d", fontSize: "50px" }} />
+                    </p>
+                  </div>
+
+                  <hr className="my-4" />
+
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    type="submit"
+                    style={{ marginBottom: "20px" }}
+                    onClick={logoutUser}
+                  >
+                    LOG OUT
+                  </button>
+
+                  <br />
+
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    type="submit"
+                    onClick={redirectHome}
+                  >
+                    CANCEL
+                  </button>
+
+                  <hr className="my-4" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
