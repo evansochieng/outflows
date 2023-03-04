e0d91b0784653bf608fad1a98ccf3d5aea50a413
evans ochieng <evansochieng121@gmail.com>
2023-03-04 06:31:21 +0300

    create skeleton code for other crud operations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,26 +16,84 @@ function App() {
   const [expenses, setExpenses] = useState([]);
 
   // define variables for total income, total expenses, and balance
-  const totalIncome = incomes.reduce( (initTotalIncome, curIncome) => initTotalIncome + curIncome.value, 0);
-  const totalExpense = expenses.reduce( (initTotalExpense, curExpense) => initTotalExpense + curExpense.value, 0);
+  const totalIncome = incomes.reduce(
+    (initTotalIncome, curIncome) => initTotalIncome + curIncome.value,
+    0
+  );
+  const totalExpense = expenses.reduce(
+    (initTotalExpense, curExpense) => initTotalExpense + curExpense.value,
+    0
+  );
   const balance = totalIncome - totalExpense;
 
-  // make fetch request to get expenses from django api
-  useEffect( () => {
+  // CRUD Operations
+  // GET - make fetch request to get expenses from django api
+  useEffect(() => {
     fetch("http://127.0.0.1:8000/api/expenses/")
       .then((res) => res.json())
       .then((data) => setExpenses(data))
-      .catch(err => console.log(err));
-  }, [])
+      .catch((err) => console.log(err));
+  }, []);
+
+  // CREATE - Add a new expense
+  const handleSubmit = () => {
+    fetch("http://127.0.0.1:8000/api/expenses/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepted": "application/json"
+      },
+      body: JSON.stringify()
+    });
+  }
+
+  // UPDATE - Edit details of an expense
+  const handleUpdate = () => {
+    fetch(`http://127.0.0.1:8000/api/expenses/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepted": "application/json"
+      },
+      body: JSON.stringify() //add what to stringify
+    });
+  }
+
+  // DELETE - Delete an expense
+  const handleDelete = () => {
+    fetch(`http://127.0.0.1:8000/api/expenses/${id}`, {
+      method: "DELETE"
+    }
+    .then(res => res.json())
+    .then(() => alert("Expense deleted successfully!")));
+  }
 
   return (
     <div className="App">
       <NavBar />
       {isLogin ? (
         <Routes>
-          <Route exact path="/home" element={<Home totalIncome={totalIncome} totalExpense={totalExpense} balance={balance} />} />
-          <Route exact path="/income" element={<Income setIncomes={setIncomes} />} />
-          <Route exact path="/expenses" element={<Expenses expenses={expenses} />} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <Home
+                totalIncome={totalIncome}
+                totalExpense={totalExpense}
+                balance={balance}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/income"
+            element={<Income setIncomes={setIncomes} />}
+          />
+          <Route
+            exact
+            path="/expenses"
+            element={<Expenses expenses={expenses} />}
+          />
           <Route
             exact
             path="/logout"
