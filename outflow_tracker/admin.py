616568a91b9a2792c8ef5b4d862b87d131892d24
evans ochieng <evansochieng121@gmail.com>
2023-03-11 03:00:06 +0300

    add income model to admin page

--- outflow_tracker/admin.py
+++ outflow_tracker/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Expense
+from .models import Income
 
 # Add the expense model to the admin page
 class ExpenseAdmin(admin.ModelAdmin):
     # list of attributes
     list = ('name', 'date', 'category', 'description', 'payment_mode', 'value')
 
+# add the income model to the admin page
+class IncomeAdmin(admin.ModelAdmin):
+    # list of attrinutes
+    list = ('name', 'date', 'source', 'amount')
+
 # register the site
 admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Income, IncomeAdmin)
