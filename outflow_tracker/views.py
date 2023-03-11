e4ac22bf5555a5ca8aced033dd0366ce2489e67c
evans ochieng <evansochieng121@gmail.com>
2023-03-11 03:19:38 +0300

    add income model to the views

--- outflow_tracker/views.py
+++ outflow_tracker/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from .serializers import ExpenseSerializer
+from .serializers import ExpenseSerializer, IncomeSerializer
 from rest_framework import viewsets
-from .models import Expense
+from .models import Expense, Income
 
 # Create view for Expenses
 class ExpenseView(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     queryset = Expense.objects.all()
+
+# create a view for Incomes
+class IncomeView(viewsets.ModelViewSet):
+    serializer_class = IncomeSerializer
+    queryset = Income.objects.all()
