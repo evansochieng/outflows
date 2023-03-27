bfebe9a6bcb0e3142abddda695e1a1b9b759cf9a
evans ochieng <evansochieng121@gmail.com>
2023-03-27 08:42:08 +0300

    add bar chart for sources of incomes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bar, Line, Pie, Doughnut } from "react-chartjs-2";
+import { Bar, Line, Doughnut } from "react-chartjs-2";
 
 //import and register all elements since chart js is treeshakable
 import { Chart, registerables } from "chart.js";
@@ -7,7 +7,7 @@ Chart.register(...registerables);
 
 const Home = ({ totalIncome, totalExpense, balance, incomes, expenses}) => {
 
-  // define a n object to store the frequencies of expense categories
+  // define an object to store the frequencies of expense categories
   const freqExpensesObject = {}
   expenses.forEach( exp => {
     // create a key for each category and value with the number of its occurence
@@ -16,20 +16,31 @@ const Home = ({ totalIncome, totalExpense, balance, incomes, expenses}) => {
       : (freqExpensesObject[exp.category] = 1);
   });
 
+  // define an object to store the total income from each income category
+  const totalCatIncome = {};
+  incomes.forEach( inc => {
+    // create a key for each category and value to be total income
+    totalCatIncome.hasOwnProperty(inc.source)
+      ? (totalCatIncome[inc.source] += inc.amount)
+      : (totalCatIncome[inc.source] = inc.amount);
+  });
+  
   ////// Visualizations with Chart JS //////
-  ////// Draw a pie chart for the expense categories ////////
+  ////// Draw a doughnut chart for the expense categories ////////
 
   //define the data
-  let data = Object.values(freqExpensesObject);
+  let expDouData = Object.values(freqExpensesObject);
   // create custom labels
   let labels = Object.keys(freqExpensesObject);
-  let customLabels = labels.map((label, index) => `${label}: ${data[index]}`);
+  let customLabels = labels.map(
+    (label, index) => `${label}: ${expDouData[index]}`
+  );
 
   const chartdata = {
     labels: customLabels,
     datasets: [
       {
-        data: data,
+        data: expDouData,
         backgroundColor: [
           "#FF6384",
           "#36A2EB",
@@ -46,10 +57,10 @@ const Home = ({ totalIncome, totalExpense, balance, incomes, expenses}) => {
     ],
   };
 
-  const options = {
+  const doughnutOptions = {
     title: {
       display: true,
-      text: "Pie Chart of Expense Categories",
+      text: "Doughnut Chart of Expense Categories",
       fontSize: 16,
     },
     legend: {
@@ -62,6 +73,41 @@ const Home = ({ totalIncome, totalExpense, balance, incomes, expenses}) => {
       color: "white",
     },
   };
+  ////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////
+  /////// Draw a bar plot of income amount from each source //////////
+  const data = {
+    labels: Object.keys(totalCatIncome),
+    datasets: [
+      {
+        label: "Total Income",
+        data: Object.values(totalCatIncome),
+        // backgroundColor: "rgba(54, 162, 235, 0.2)",
+        backgroundColor: "#36A2EB",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+    title: {
+      display: true,
+      text: "Total Income per Income Stream",
+      fontSize: 16,
+    },
+  };
+  ///////////////////////////////////////////////////////////
 
   return (
     <div>
@@ -101,9 +147,25 @@ const Home = ({ totalIncome, totalExpense, balance, incomes, expenses}) => {
         </div>
       </div>
 
-      <div style={{ maxWidth: "800px", maxHeight: "600px" }}>
-        <h2>Pie Chart of Expenses</h2>
-        <Doughnut data={chartdata} options={options} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div style={{ maxWidth: "600px", maxHeight: "400px" }}>
+          <h2 style={{ textAlign: "center" }}>
+            Doughnut Chart of Frequency of Expenses Category
+          </h2>
+          <Doughnut data={chartdata} options={doughnutOptions} />
+        </div>
+
+        <div style={{ maxWidth: "600px", maxHeight: "400px" }}>
+          <h2 style={{ textAlign: "center" }}>
+            Bar Graph of Income per Income Stream
+          </h2>
+          <Bar data={data} options={options} />
+        </div>
       </div>
     </div>
   );
